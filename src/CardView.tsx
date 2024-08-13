59391f18cab0e5ec1bd35449c6d37000cde44a04
npaby <74232124+npaby@users.noreply.github.com>
2024-08-13 21:56:04 +0800

    Cardview 1.1

--- src/CardView.tsx
+++ src/CardView.tsx
@@ -1,39 +1,92 @@
-import React from 'react';
-import {Card, CardDescription, CardContent, CardHeader, CardTitle,} from "./components/ui/card";
-import {Badge} from "./components/ui/badge";
+import * as React from 'react';
+import {Card, CardContent, CardHeader, CardTitle,} from "./components/ui/card";
 import {Button} from "./components/ui/button";
 import {useNavigate} from "react-router-dom";
 import {ScrollArea} from "./components/ui/scroll-area";
+import {Dialog, DialogContent, DialogTitle, DialogDescription, DialogClose, DialogTrigger, DialogHeader} from "./components/ui/dialog";
 const CardView = ({table}) => {
     const filteredRows = table.getPaginationRowModel().rows;
     const navigate = useNavigate();
+    const cmosh = "CMOSH".split("");
+
     const renderMembers = (members) => {
-        return members.map((member) => (
-            <div key={member.user_id} className="mb-2">
-                <div className="grid grid-cols-[minmax(50px,auto)_1fr]">
-                    <div className={"text-right"}>
-                        {member.user_id && member.user_owner ? (
-                            <b>{member.user_id}</b>
-                        ) : (
-                            <span>{member.user_id}</span>
-                        )}
-                    </div>
-                    <Badge className="ml-2">{member.user_role}</Badge>
-                </div>
+        const [selectedMember, setSelectedMember] = React.useState(null);
+
+        const handleClickOpen = (member) => {
+            setSelectedMember(member);
+        };
+        const handleOnClose = () => {
+            setSelectedMember(null);
+        };
+        return (
+            <div className="flex flex-row justify-center">
+                {cmosh.map((char, index) => {
+                    const matchingMember = members.find(
+                        (member) => member.user_role.slice(0, 1).toUpperCase() === char.toUpperCase()
+                    );
+                    return (
+                        <Dialog key={`${char}-${index}`}>
+                            <DialogTrigger asChild>
+                                <div
+                                    className={`inline-block border-solid w-12 h-12 border-2 rounded-full align-middle flex items-center justify-center ${matchingMember ? 'bg-red-500' : 'bg-blue-500'} text-white cursor-pointer`}
+                                    onClick={() => matchingMember && handleClickOpen(matchingMember) && selectedMember !== null}
+                                >
+                                    {char}
+                                </div>
+                            </DialogTrigger>
+                            <DialogContent className={"bg-white w-64"}>
+                                <DialogHeader>
+                                    <DialogTitle>Member Information</DialogTitle>
+
+                                </DialogHeader>
+                                <div>
+                                    User ID: {selectedMember?.user_id}
+                                </div>
+                                <div>
+                                    User Role: {selectedMember?.user_role}
+                                </div>
+                                <Button className={"mt-4"}>Add Friend</Button>
+                                <DialogClose asChild>
+                                    <Button className={" w-full"} onClick={() => handleOnClose()}>Close</Button>
+                                </DialogClose>
+                            </DialogContent>
+                        </Dialog>
+                    );
+                })}
             </div>
-
-        ));
+        );
     };
-
+    {
+        /*
+                    <div className="flex flex-row justify-center">
+                        {cmosh.map((char, index) => {
+                            const matchingMember = members.find(member =>
+                                member.user_role.slice(0, 1).toUpperCase() === char.toUpperCase()
+                            );
+                            return matchingMember ? (
+                                <div key={`${char}-${matchingMember.user_role}`}
+                                     className="inline-block border-solid bg-blue-500 w-12 h-12 border-2 rounded-full align-middle flex items-center justify-center bg-blue-900 text-white">
+                                    {char}
+                                </div>
+                            ) : (
+                                <div key={`${char}-nonexistent`}
+                                     className="inline-block border-solid bg-blue-500 w-12 h-12 border-2 rounded-full align-middle flex items-center justify-center bg-red-500 text-black">
+                                    {char}
+                                </div>
+                            );
+                        })}
+                    </div>
+       */
+    }
     return (
         <ScrollArea className="h-[70lvh] p-10">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 {filteredRows.map((row) => {
                     const lobby = row.original;
                     return (
                         <Card key={lobby.Lobby_Id} className="shadow-md">
                             <CardHeader>
-                            <CardTitle className={"mt-4"}>{lobby.Lobby_Name}</CardTitle>
+                                <CardTitle className={"mt-4"}>{lobby.Lobby_Name}</CardTitle>
                                 <div className="text-sm m-2 text-gray-500">{lobby.Lobby_Id}</div>
                             </CardHeader>
                             <CardContent>
@@ -50,8 +103,8 @@ const CardView = ({table}) => {
                                 <div className="text-3xl text font-black">
                                     {lobby.Rank}
                                 </div>
-                                <div className={"mt-8 h-44 "}>
-                                {renderMembers(lobby.Members)}
+                                <div className={"mt-4 mb-4"}>
+                                    {renderMembers(lobby.Members)}
                                 </div>
                                 <Button className="w-full text-white bg-blue-500 hover:bg-"
                                         onClick={() =>
